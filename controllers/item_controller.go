14f70acd81321331657a0615baf6ae947c00a757
Ryan Wilcox <rwilcox11@icloud.com>
2026-07-26 06:53:28 -0400

    Fixed GetItems bug by removing 'preload' from the query

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -36,7 +36,7 @@ func GetItems(c *gin.Context) {
 	//TODO: Add filtering
 
 	// Fetch all items from the database.
-	if err := db.Preload("Item").Find(&items).Error; err != nil {
+	if err := db.Find(&items).Error; err != nil {
 		c.Error(err)
 		return
 	}
